c1de03f62f5996a682cd8e61a83bd28adcfc4606
chiiymt <129878695+chiiymt@users.noreply.github.com>
2023-04-26 14:13:16 +0000

    [Add] フラッシュメッセージ

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,20 +4,13 @@ class ListsController < ApplicationController
   end
 
   def create
-    #データを受け取る＆新規登録のためのインスタンスを作成
-    @list = List.new(list_params)
-    
-    #データをデータベースに保存するためのメソッド(saveメソッド)を実行
-    if @list.save
-    
-    #詳細画面へリダイレクト
-    redirect_to list_path(@list.id)
-    
-    else
-      render :new
-    end
-    
-    
+     # １. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 2. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 3. フラッシュメッセージを定義し、詳細画面へリダイレクト
+    flash[:notice] = "投稿が成功しました"
+    redirect_to list_path(list.id)
   end
 
   def index
